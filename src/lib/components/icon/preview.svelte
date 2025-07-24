4ef4b3919f9eea94b82997632d6b4da3f885080d
alexis <179990131+gvrciary@users.noreply.github.com>
2025-07-24 01:26:28 -0700

    fix: noise

--- src/lib/components/icon/preview.svelte
+++ src/lib/components/icon/preview.svelte
@@ -49,10 +49,16 @@
 		uniform vec3 u_colors[10];
 		uniform vec2 u_positions[10];
 		uniform int u_numberPoints;
+		uniform float u_noiseRatio;
+
+		float rand(vec2 n) {
+			return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+		}
 
 		void main() {
 			vec2 st = gl_FragCoord.xy/u_resolution.xy;
 			st.y=1.-st.y;
+			vec3 noise=vec3(rand(vec2(st.x*5.,st.y*5.)));
 
 			float pointGradient=0.;
 			vec3 colorGradient=vec3(0.);
@@ -72,7 +78,7 @@
 			totalLight=smoothstep(0.,1.,clamp(1.-totalLight,0.,1.));
 			colorGradient=(colorGradient/pointGradient)*totalLight;
 			vec3 bgGradient=(1.-totalLight)*u_bgColor;
-			vec3 total=clamp(colorGradient,0.,1.)+bgGradient;
+			vec3 total=mix(clamp(colorGradient,0.,1.)+bgGradient,noise,u_noiseRatio);
 			gl_FragColor = vec4(vec3(total),1.);
 		}
 	`;
@@ -188,6 +194,7 @@
 		gl.uniform3fv(gl.getUniformLocation(shaderProgram, 'u_colors'), colors);
 		gl.uniform2fv(gl.getUniformLocation(shaderProgram, 'u_positions'), positions);
 		gl.uniform1i(gl.getUniformLocation(shaderProgram, 'u_numberPoints'), numPoints);
+		gl.uniform1f(gl.getUniformLocation(shaderProgram, 'u_noiseRatio'), ($noise / 100) * 0.3);
 
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 	}
@@ -305,7 +312,7 @@
 					</radialGradient>
 				{/if}
 
-				{#if $noise > 0 && $backgroundType !== 'mesh'}
+				{#if $noise > 0}
 					<filter id={noiseId}>
 						<feTurbulence
 							baseFrequency={0.4 + ($noise / 100) * 0.6}
